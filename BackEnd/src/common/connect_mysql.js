7020e75fa9aacf58f85f69bbdf2bc618221aae5b
Nguyen Tien Dat <dat86868389@gmail.com>
2023-05-11 23:40:30 +0700

    backup backend

--- BackEnd/src/common/connect_mysql.js
+++ BackEnd/src/common/connect_mysql.js
@@ -2,13 +2,15 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'zxcVbnm123@',
-    database: 'doan2023'
+    password: 'zxcVbnm123',
+    database: 'doan2023',
+    port: 3306
 })
 
 connection.connect(function (err) {
     if (err) {
-        console.log("Ket noi Co So Du Lieu That Bai");
+        throw err;
+        // console.log("Ket noi Co So Du Lieu That Bai");
     }
     else {
         console.log("ket noi CSDL MySQL thanh cong");
