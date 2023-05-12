da310c49cc7ce498dac3828985b16482db7b4b6c
Nguyen Tien Dat <dat86868389@gmail.com>
2023-05-12 15:44:54 +0700

    update paggination

--- web-gioi-thieu-game/src/components/paging_component.js
+++ web-gioi-thieu-game/src/components/paging_component.js
@@ -3,30 +3,133 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 
+export default function pagging({
+    currentPage
+}) {
+    const totalPages = 12;
+    const midSide = 2;
+    const leftSide = 3;
+    const rightSide = 3;
+    const recordsPerPage = 8;
 
 
 
 
 
 
-export default function pagging( {currentPage, totalPages}) {
-
-    
-    return(
+    return (
         <div className="row pagging-container">
             <div className="offset-xl-3 col-xl-6 pagging-container_buttons">
-                <Link href='1'><FontAwesomeIcon icon={faChevronLeft} /></Link>
-                <Link href='2'>1</Link>
-                <Link href='2'>2</Link>
-                <span>...</span>
-                <Link href='2'>6</Link>
-                <span className="active">7</span>
-                <Link href='2'>8</Link>
-                <span>...</span>
-                <Link href='2'>328</Link>
-                <Link href='2'>329</Link>
-                <Link href='1'><FontAwesomeIcon icon={faChevronRight} /></Link>
+                {handlePagging(currentPage, totalPages, leftSide, rightSide, midSide)}
             </div>
         </div>
     )
+
+}
+
+
+function handlePagging(currentPage, totalPages, leftSide, rightSide, midSide) {
+    let arrayPages = [];
+    let _currentPage = parseInt(currentPage)
+    // totalPages < leftSide + rightSide + midSide
+    if (totalPages <= (leftSide + rightSide + midSide)) {
+        arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) - 1}`}><FontAwesomeIcon icon={faChevronLeft} /></Link>);
+        for (let index = 1; index <= totalPages; index++) {
+            if (index == currentPage) {
+                arrayPages.push(<span className="active">{index}</span>);
+            }
+            else if (index > 0 && index <= totalPages) {
+                arrayPages.push(<Link href={`/new_posts/${index}`}>{index}</Link>);
+            }
+
+        }
+        arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) + 1}`}><FontAwesomeIcon icon={faChevronRight} /></Link>);
+    }
+
+    else {
+        if (currentPage < leftSide) {
+            arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) - 1}`}><FontAwesomeIcon icon={faChevronLeft} /></Link>);
+            for (let i = 1; i <= leftSide; i++) {
+                if (i == currentPage) {
+                    arrayPages.push(<span className="active">{i}</span>)
+                }
+                else {
+                    arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                }
+            }
+            arrayPages.push(<span>...</span>)
+            for (let i = totalPages - rightSide + 1; i <= totalPages; i++) {
+                arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+            }
+            arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) + 1}`}><FontAwesomeIcon icon={faChevronRight} /></Link>);
+        }
+
+        if (currentPage >= leftSide && currentPage < totalPages - (rightSide + midSide) + 1) {
+            if (currentPage <= leftSide + midSide) {
+                arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) - 1}`}><FontAwesomeIcon icon={faChevronLeft} /></Link>);
+                let total = parseInt(parseInt(currentPage) + 1);
+                for (let i = 1; i <= total; i++) {
+
+                    if (i == currentPage) {
+                        arrayPages.push(<span className="active">{i}</span>)
+                    }
+                    else {
+                        arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                    }
+                }
+                arrayPages.push(<span>...</span>)
+                for (let i = totalPages - rightSide + 1; i <= totalPages; i++) {
+                    arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                }
+                arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) + 1}`}><FontAwesomeIcon icon={faChevronRight} /></Link>);
+            }
+
+            if (currentPage > (leftSide + midSide)) {
+
+                arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) - 1}`}><FontAwesomeIcon icon={faChevronLeft} /></Link>);
+                let total = parseInt(parseInt(currentPage) + 1);
+                for (let i = 1; i <= leftSide; i++) {
+                    arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                }
+                arrayPages.push(<span>...</span>)
+                for (let i = _currentPage - 1; i <= _currentPage + 1; i++) {
+                    if (i == currentPage) {
+                        arrayPages.push(<span className="active">{i}</span>);
+                    }
+                    else {
+                        arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                    }
+                }
+                arrayPages.push(<span>...</span>)
+                for (let i = totalPages - rightSide + 1; i <= totalPages; i++) {
+                    arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                }
+                arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) + 1}`}><FontAwesomeIcon icon={faChevronRight} /></Link>);
+            }
+        }
+
+        if (currentPage >= totalPages - (rightSide + midSide) + 1 && currentPage <= totalPages) {
+
+            arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) - 1}`}><FontAwesomeIcon icon={faChevronLeft} /></Link>);
+            for (let i = 1; i <= leftSide; i++) {
+                arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+            }
+            arrayPages.push(<span>...</span>)
+            for (let i = _currentPage - ((midSide + rightSide - 1) - (totalPages - _currentPage)); i <= totalPages; i++) {
+                if (i == _currentPage) {
+                    arrayPages.push(<span className="active">{i}</span>);
+                }
+                else {
+                    arrayPages.push(<Link href={`/new_posts/${i}`}>{i}</Link>);
+                }
+            }
+            arrayPages.push(<Link href={`/new_posts/${parseInt(currentPage) + 1}`}><FontAwesomeIcon icon={faChevronRight} /></Link>);
+
+        }
+
+    }
+
+
+
+    return arrayPages;
 }
--- web-gioi-thieu-game/src/pages/new_posts/[page]/index.js
+++ web-gioi-thieu-game/src/pages/new_posts/[page]/index.js
@@ -1,10 +1,6 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import { useRouter } from 'next/router'
-import {
-    faMagnifyingGlass
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Import components
 import NavBarComponent from '@/components/navbar_component';
@@ -12,18 +8,17 @@ import Footer from '@/components/footer.js';
 import NewPostsComponent from '@/components/new_posts_component';
 import Header from '@/components/header';
 import Pagging from '@/components/paging_component';
+import { useEffect, useState } from 'react';
 
 
 export default function NewsPosts() {
-
     const router = useRouter();
-    const { page } = router.query;
+    const { page } = router.query;  
+
     return (
         <div className='container-lg'>
             <Head>
                 <title>Bài viết mới</title>
-                <link rel="icon" type="image/png" izes="192x192" href="/favicon-220x220.png" />
-                <link rel="icon" type="image/png" izes="32x32" href="/favicon-220x220.png" />
             </Head>
 
             <Header />
@@ -32,9 +27,18 @@ export default function NewsPosts() {
 
             <NewPostsComponent />
 
-            <Pagging />
+            {
+                page != '' && (
+                    <Pagging
+                        currentPage={page}
+                    />
+                )
+            }
+
             <Footer />
         </div>
 
     );
+
+
 }
