a7d73d19e5a8a23b90f7ba6fd6ef1850431b9a79
Nguyen Tien Dat <dat86868389@gmail.com>
2023-05-17 18:53:57 +0700

    check user logged on personal post

--- web-gioi-thieu-game/src/pages/personal_post/[idUser]/index.js
+++ web-gioi-thieu-game/src/pages/personal_post/[idUser]/index.js
@@ -3,27 +3,40 @@ import Layout from "@/layouts/layout";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import useUser from "@/lib/useUser";
 
 
 
 export default function PesonalPost() {
     const router = useRouter();
     const { idUser } = router.query;
+    const { user } = useUser();
+
 
     useEffect(() => {
+        if (user?.isLoggedIn === false) {
+            router.push('/login');
+        }
 
     }, [router.isReady])
+    
 
-    if (router.isReady) {
-        return (
-            <Layout>
-                <Head>
-                    <title>Bài viết cá nhân</title>
-                </Head>
-                <PesonalPostsComponent id={idUser} />
-            </Layout>
-
-        );
+    if (router.isReady && user?.isLoggedIn) {
+        if (user.userId != idUser) {
+            router.push('/');
+        }
+        else {
+            return (
+                <Layout>
+                    <Head>
+                        <title>Bài viết cá nhân</title>
+                    </Head>
+                    <PesonalPostsComponent id={idUser} />
+                </Layout>
+    
+            );
+        }
+       
     }
 
 }
