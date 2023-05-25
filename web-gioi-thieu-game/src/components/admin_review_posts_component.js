15325f8728f5c668596574cb71aa569d14781d8e
Nguyen Tien Dat <dat86868389@gmail.com>
2023-05-26 01:18:28 +0700

    update

--- web-gioi-thieu-game/src/components/admin_review_posts_component.js
+++ web-gioi-thieu-game/src/components/admin_review_posts_component.js
@@ -2,88 +2,86 @@ import { useEffect, useState } from "react";
 import PostsMagementStyle from '../styles/admin_post_management.module.css';
 import Head from "next/head";
 import Link from "next/link";
-
+import useSWR from 'swr';
+import AdminPaginationComponent from "./admin_pagination_component";
 
 
 
 export default function AdminReviewPostsComponent({ page }) {
-    const [posts, setPosts] = useState();
-
-    useEffect(() => {
-        fetch(`http://localhost:3001/posts/status/code/0/page/${page}`)
-            .then(response => response.json())
-            .then(e => {
-                console.log(e);
-                setPosts(e.result)
-            })
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+    const posts = useSWR(`http://localhost:3001/posts/status/code/0/page/${page}`, fetcher);
+    const posts_quantity = useSWR('http://localhost:3001/posts/status/code/0/quantity', fetcher);
 
 
-    }, [page]);
 
+    if (posts.data && posts_quantity.data) {
+        console.log(posts_quantity?.data?.result[0].count);
+        return (
+            <div className={`${PostsMagementStyle.container}`}>
+                <Head>
+                    <title>Duệt bài viết</title>
+                </Head>
+                <h2>Quản lý bài viết</h2>
+                <div className="row">
+                    <div className="col-12">
+                        <table className={`${PostsMagementStyle.table}`}>
+                            <thead>
+                                <tr className={`${PostsMagementStyle.tr}`}>
+                                    <th className={`${PostsMagementStyle.th}`}>Tên bài viêt</th>
+                                    <th className={`${PostsMagementStyle.th}`}>Tác giả</th>
+                                    <th className={`${PostsMagementStyle.th}`}>Ngày đăng</th>
+                                    <th className={`${PostsMagementStyle.th}`}>Duyệt</th>
+                                    <th className={`${PostsMagementStyle.th}`}>Không Duyệt</th>
 
-    return (
-        <div className={`${PostsMagementStyle.container}`}>
-            <Head>
-                <title>Duệt bài viết</title>
-            </Head>
-            <h2>Quản lý bài viết</h2>
-            <div className="row">
-                <div className="col-12">
-                    <table className={`${PostsMagementStyle.table}`}>
-                        <thead>
-                            <tr className={`${PostsMagementStyle.tr}`}>
-                                <th className={`${PostsMagementStyle.th}`}>Tên bài viêt</th>
-                                <th className={`${PostsMagementStyle.th}`}>Tác giả</th>
-                                <th className={`${PostsMagementStyle.th}`}>Ngày đăng</th>
-                                <th className={`${PostsMagementStyle.th}`}>Duyệt</th>
-                                <th className={`${PostsMagementStyle.th}`}>Không Duyệt</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    posts?.data?.result.map(p => (
+                                        <tr className={`${PostsMagementStyle.tr}`}>
+                                            <td className={`${PostsMagementStyle.td}`}>
+                                                <Link href={`/admin/preview_post/${p.PostId}`} target="blank" >
+                                                    {p.Title}
+                                                </Link>
+                                            </td>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                posts?.map(p => (
-                                    <tr className={`${PostsMagementStyle.tr}`}>
-                                        <td className={`${PostsMagementStyle.td}`}>
-                                            <Link href={`/admin/preview_post/${p.PostId}`} target="blank" >
-                                                {p.Title}
-                                            </Link>
-                                        </td>
+                                            <td className={`${PostsMagementStyle.td}`}>
+                                                {p.UserName}
+                                            </td>
 
-                                        <td className={`${PostsMagementStyle.td}`}>
-                                            {p.UserName}
-                                        </td>
+                                            <td className={`${PostsMagementStyle.td}`}>
+                                                {handleRenderDate(p.TimePost)}
+                                            </td>
 
-                                        <td className={`${PostsMagementStyle.td}`}>
-                                            {handleRenderDate(p.TimePost)}
-                                        </td>
+                                            <td className={`${PostsMagementStyle.td}`}>
+                                                <button onClick={() => { handleDelete(p.PostId) }}>Duyệt</button>
+                                            </td>
 
-                                        <td className={`${PostsMagementStyle.td}`}>
-                                            <button onClick={() => { handleDelete(p.PostId) }}>Duyệt</button>
-                                        </td>
+                                            <td className={`${PostsMagementStyle.td}`}>
+                                                <button onClick={() => { handleDelete(p.PostId) }}>Không Duyệt</button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
 
-                                        <td className={`${PostsMagementStyle.td}`}>
-                                            <button onClick={() => { handleDelete(p.PostId) }}>Không Duyệt</button>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
+                        </table>
+                    </div>
+                </div>
 
-                    </table>
+                <div className='row'>
+                    <div className='col-12'>
+                        <AdminPaginationComponent
+                            currentPage={page}
+                            totalPages={Math.ceil(posts_quantity?.data?.result[0].count / 12)}
+                            subDomain={`/admin/review_post/`}
+                        />
+                    </div>
                 </div>
             </div>
+        )
+    }
 
-            {/* <div className='row'>
-                <div className='col-12'>
-                    <AdminPaginationComponent
-                        currentPage={page}
-                        totalPages={Math.ceil(posts_quantity?.data?.result[0].quantity / 15)}
-                    />
-                </div>
-            </div> */}
-        </div>
-    )
 }
 
 
