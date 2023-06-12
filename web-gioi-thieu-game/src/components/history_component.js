e3ace30bff691e848dfb98ff8730471b34b75184
Nguyen Tien Dat <dat86868389@gmail.com>
2023-06-12 17:55:53 +0700

    Update history_component.js

--- web-gioi-thieu-game/src/components/history_component.js
+++ web-gioi-thieu-game/src/components/history_component.js
@@ -1,42 +1,51 @@
 import { useEffect, useState } from 'react';
 import PersonalPostStyle from '../styles/pesonal_posts_component.module.css';
 import Link from 'next/link';
+import useUser from '@/lib/useUser';
+import { useRouter } from 'next/router';
 
 
 export default function HistoryComponent({ idUser }) {
     const [history, setHistory] = useState();
-
+    const { user } = useUser();
+    const router = useRouter();
     useEffect(() => {
         fetch(`http://localhost:3001/history/get/${idUser}`)
-        .then((response) => response.json())
-        .then(e=> {
-            setHistory(e);
-        })
+            .then((response) => response.json())
+            .then(e => {
+                setHistory(e);
+            })
     }, [])
 
-    return (
-        <div className={`row ${PersonalPostStyle.container}`}>
-            <h1>Các bài viết đã xem</h1>
-            {
-                history?.result.map((e) => (
-                    <div className={`col-3 ${PersonalPostStyle.items}`}>
-                        <div className={PersonalPostStyle.thumbnail}>
-                            <Link href={`/posts/${e.PostId}`}>
-                                <img src={`${e.Thumnail}`} />
-                            </Link>
-                        </div>
+    if (user?.isLoggedIn == false || user?.userId != idUser) {
+        router.push("/");
+    }
+    else {
+        return (
+            <div className={`row ${PersonalPostStyle.container}`}>
+                <h1>Các bài viết đã xem</h1>
+                {
+                    history?.result.map((e) => (
+                        <div className={`col-3 ${PersonalPostStyle.items}`}>
+                            <div className={PersonalPostStyle.thumbnail}>
+                                <Link href={`/posts/${e.PostId}`}>
+                                    <img src={`${e.Thumnail}`} />
+                                </Link>
+                            </div>
 
-                        <div className='content'>
-                            <h3>{e.Title}</h3>
-                            <p>{e.description}</p>
-                            <p>Ngày xem: {handleRenderDate(e.date)}</p>
-                        </div>
+                            <div className='content'>
+                                <h3>{e.Title}</h3>
+                                <p>{e.description}</p>
+                                <p>Ngày xem: {handleRenderDate(e.date)}</p>
+                            </div>
 
-                    </div>
-                ))
-            }
-        </div>
-    );
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
+
 }
 
 
@@ -49,7 +58,7 @@ function handleRenderDate(time) {
     const hour = dateObject.getHours() < 10 ? `0${dateObject.getHours()}` : dateObject.getHours();
     const minute = dateObject.getMinutes() < 10 ? `0${dateObject.getMinutes()}` : dateObject.getMinutes();
     const second = dateObject.getSeconds() < 10 ? `0${dateObject.getSeconds()}` : dateObject.getSeconds();
-    
+
     return `${date}-${month}-${year} ${hour}:${minute}:${second}`;
 
 }
